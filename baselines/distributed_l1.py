1a5e68998845447d07e5d039a8be6cc6e56a9007
Yifan <632181372@qq.com>
2024-04-25 17:32:30 -0400

    new file:   baselines/distributed_l1.py

---
+++ baselines/distributed_l1.py
@@ -0,0 +1,7 @@
+"""
+Project:    TII - MAS Fault Detection, Identification, and Reconfiguration
+Author:     Yifan Guo
+Description:
+            - Implementation of " Distributed l1-State-and-Fault Estimation for Multiagent Systems".
+"""
+
